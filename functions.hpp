2bd12973b2df6229fc4e4058ce3e5726b58e4863
Matheus Cardoso <dacachmatheus@gmail.com>
2022-07-22 19:46:25 -0300

    Add split into chunks

---
+++ functions.hpp
@@ -0,0 +1,42 @@
+//
+// Created by matheus on 22/07/22.
+//
+
+#ifndef ROLLING_HASH_FILE_DIFF_FUNCTIONS_HPP
+#define ROLLING_HASH_FILE_DIFF_FUNCTIONS_HPP
+
+#include <cmath>
+#include <string>
+#include <vector>
+
+// TODO: restructure project
+namespace functions
+{
+    auto split_into_chunks(const std::string& input_string, const std::size_t chunk_size) -> std::vector<std::string>
+    {
+        if (std::size(input_string) <= chunk_size)
+            return { input_string };
+
+        auto result = std::vector<std::string>{};
+        auto number_of_chunks = std::size(input_string) / chunk_size;
+        if (std::size(input_string) % chunk_size != 0)
+            ++number_of_chunks;
+        result.reserve(number_of_chunks);
+        for (std::size_t current_index = 0; current_index < std::size(input_string);)
+        {
+            auto current_string = std::string{};
+            current_string.reserve(chunk_size);
+            for (std::size_t offset = 0; offset < chunk_size; ++offset)
+            {
+                if (current_index + offset >= std::size(input_string))
+                    break;
+                current_string.push_back(input_string.at(current_index + offset));
+            }
+            current_index += chunk_size;
+            result.push_back(current_string);
+        }
+        return result;
+    }
+} // namespace functions
+
+#endif // ROLLING_HASH_FILE_DIFF_FUNCTIONS_HPP
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,5 +1,88 @@
+#include "../functions.hpp"
 #include "catch.hpp"
 
-TEST_CASE() {
-    REQUIRE(2 + 2 == 4);
+TEST_CASE("Strings are split into chunks")
+{
+    GIVEN("A string and a chunk size")
+    {
+        const auto original_string = std::string{ "Whatever our souls are made of, his and mine are the same." };
+        WHEN("The chunk size is bigger than the string length")
+        {
+            const auto chunk_size = std::size_t{ 200 };
+            THEN("The result is the whole string as a chunk")
+            {
+                const auto split_string = functions::split_into_chunks(original_string, chunk_size);
+                REQUIRE(std::size(split_string) == 1);
+                REQUIRE(split_string.front() == original_string);
+            }
+        }
+
+        WHEN("The chunk size is one")
+        {
+            const auto chunk_size = std::size_t{ 1 };
+            THEN("The result is each letter as a chunk")
+            {
+                const auto split_string = functions::split_into_chunks(original_string, chunk_size);
+                REQUIRE(std::size(split_string) == std::size(original_string));
+                auto is_same_letter = [](auto letter, const auto& letter_as_string)
+                {
+                    return letter == letter_as_string.front();
+                };
+                REQUIRE(std::ranges::equal(original_string, split_string, is_same_letter));
+            }
+        }
+
+        WHEN("The chunk size is less than the string length")
+        {
+            AND_WHEN("The string length is a multiple of chunk size")
+            {
+                const auto chunk_size = std::size_t{ 2 };
+                THEN("All chunks have the same size")
+                {
+                    const auto split_string = functions::split_into_chunks(original_string, chunk_size);
+                    for (const auto& chunk : split_string)
+                        REQUIRE(std::size(chunk) == chunk_size);
+                }
+            }
+            AND_WHEN("The string length is not a multiple of chunk size")
+            {
+                const auto chunk_size = std::size_t{ 10 };
+                THEN("All chunks but the last have the same size")
+                {
+                    auto split_string = functions::split_into_chunks(original_string, chunk_size);
+                    const auto& last_chunk = split_string.back();
+                    split_string.pop_back();
+                    for (const auto& chunk : split_string)
+                        REQUIRE(std::size(chunk) == chunk_size);
+                    REQUIRE(std::size(last_chunk) != chunk_size);
+                    REQUIRE(std::size(last_chunk) == 8);
+                }
+            }
+        }
+    }
+
+    GIVEN("A 37-length string")
+    {
+        const auto original_string = std::string{ "Do not go gentle into that good night" };
+
+        WHEN("Chunk size is 5")
+        {
+            const auto chunk_size = std::size_t{ 5 };
+            const auto split_string = functions::split_into_chunks(original_string, chunk_size);
+            THEN("Chunks are substrings of length 5")
+            {
+                REQUIRE(split_string.at(0) == "Do no");
+                REQUIRE(split_string.at(1) == "t go ");
+                REQUIRE(split_string.at(2) == "gentl");
+                REQUIRE(split_string.at(3) == "e int");
+                REQUIRE(split_string.at(4) == "o tha");
+                REQUIRE(split_string.at(5) == "t goo");
+                REQUIRE(split_string.at(6) == "d nig");
+            }
+            AND_THEN("Last chunk is of length 2")
+            {
+                REQUIRE(split_string.at(7) == "ht");
+            }
+        }
+    }
 }
