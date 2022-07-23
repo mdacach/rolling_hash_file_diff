bf14175fdcd5264e8072989ba370af02740a20f1
Matheus Cardoso <dacachmatheus@gmail.com>
2022-07-23 14:06:45 -0300

    Add reading file inputs
    This way we can test the functionality as CLI application.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
+#include <fstream>
 #include <iostream>
+#include <sstream>
 
-int main()
+auto read_file_to_string(const std::string& file_path) -> std::string;
+
+auto main(int argc, char* argv[]) -> int
 {
-    std::cout << "Hello world from Docker!\n";
+    std::cout << "You have entered " << argc << " arguments: " << '\n';
+    for (int i = 0; i < argc; ++i)
+        std::cout << argv[i] << '\n';
+    auto first_file = read_file_to_string(argv[1]);
+    auto second_file = read_file_to_string(argv[2]);
+    std::cout << "First file: " << first_file << '\n';
+    std::cout << "Second file: " << second_file << '\n';
     return 0;
+}
+
+auto read_file_to_string(const std::string& file_path) -> std::string
+{
+    auto input_file = std::ifstream{ file_path };
+    if (!input_file)
+        throw std::runtime_error("Could not open file\n");
+    auto stream = std::stringstream{};
+    stream << input_file.rdbuf();
+    return stream.str();
 }
