6029d5f2d3d00aa52a3a7a15682da2f00a48210e
Matheus Cardoso <dacachmatheus@gmail.com>
2022-07-24 13:24:18 -0300

    Update test script to comply with CLI interface

--- tests/tester_script.py
+++ tests/tester_script.py
@@ -10,20 +10,31 @@ for test_number in range(1, 101):
     file2 = "test_files/test" + str(test_number) + "/file2"
 
     # Calls the executable passing file1 and file2 as arguments
-    subprocess.run(["../build/rolling_hash_file_diff", file1, file2])
+    # Create the signature from file1
+    subprocess.run(["../build/rolling_hash_file_diff", "signature", file1, "signature_file"])
+    signature_file = open("signature_file", "r")
+    # print(signature_file.read())
 
-    # After this call, we have created a `result` file (what file1 becomes after the whole process)
-    # If all went well, `result` should be equal to `file2`
-    # Note that result is created outside of the specific test directory
-    # and is reused between tests
-    if filecmp.cmp(file2, "result"):
+    # Create the delta from file2 regarding signature
+    subprocess.run(["../build/rolling_hash_file_diff", "delta", "signature_file", file2, "delta_file"])
+    delta_file = open("delta_file", "r")
+    # print(delta_file.read())
+
+    # Patch file1 with delta
+    subprocess.run(["../build/rolling_hash_file_diff", "patch", file1, "delta_file", "reconstructed_file"])
+    reconstructed_file = open("reconstructed_file", "r")
+    # print(reconstructed_file.read())
+
+    # If all went well, `reconstructed_file` should be equal to `file2`
+    if filecmp.cmp(file2, "reconstructed_file"):
         print("All went well")
     else:
         print("Hash collision here")
         bad_counter += 1
 
-    print(os.stat(file1))
-    print(os.stat(file2))
-    print(os.stat("result"))
+    print("*********** Statistics ***********")
+    print(f"File2 size: {os.stat(file2).st_size}")
+    print(f"Signature size: {os.stat('signature_file').st_size}")
+    print(f"Delta size: {os.stat('delta_file').st_size}")
 
-print("Failures: ", bad_counter)
+    print("Failures: ", bad_counter)
