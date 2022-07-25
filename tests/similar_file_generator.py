21df7227c388c1beb763a7836c0dde1802c8ebd3
Matheus Cardoso <dacachmatheus@gmail.com>
2022-07-25 13:53:37 -0300

    Update python test scripts

--- tests/similar_file_generator.py
+++ tests/similar_file_generator.py
@@ -1,12 +1,14 @@
 # Create similar files to be used when testing the file diff program
+import os
 import random
 import string
 
 
-def create_random_chunks(number_chunks, size_chunks):
+def create_random_chunks(number_chunks, size_chunks, human_readable=False):
     chunks = []
     for i in range(number_chunks):
-        random_chunk = ''.join(random.choice(string.printable) for _ in range(size_chunks))
+        random_chunk = ''.join(
+            random.choice(string.ascii_letters if human_readable else string.printable) for _ in range(size_chunks))
         chunks.append(random_chunk)
 
     print(f'chunks: {chunks}')
@@ -24,20 +26,75 @@ def generate_file(iterations, chunk_probability, chunks):
     return file
 
 
-tests_to_generate = 100
-for i in range(tests_to_generate):
-    directory_path = "test_files/test" + str(i + 1)
-    # os.makedirs(directory_path)
+def generate_big_files_for_automated_test(directory_path):
+    tests_to_generate = 100
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
 
-    number_random_chunks = random.randint(1, 50)
-    size_chunks = random.randint(1, 500)
-    created_chunks = create_random_chunks(number_random_chunks, size_chunks)
+    for i in range(tests_to_generate):
+        path_to_save = directory_path + "/test" + str(i + 1)
+        if not os.path.exists(path_to_save):
+            os.makedirs(path_to_save)
 
-    file1_iterations = random.randint(1, 1000)
-    file2_iterations = random.randint(1, 1000)
-    file1_chunk_probability = random.random()
-    file2_chunk_probability = random.random()
-    file1 = generate_file(file1_iterations, file1_chunk_probability, created_chunks)
-    file2 = generate_file(file2_iterations, file2_chunk_probability, created_chunks)
-    open(directory_path + "/file1", "w").write(file1)
-    open(directory_path + "/file2", "w").write(file2)
+        number_random_chunks = random.randint(1, 50)
+        size_chunks = random.randint(1, 500)
+        created_chunks = create_random_chunks(number_random_chunks, size_chunks)
+
+        file1_iterations = random.randint(1, 1000)
+        file2_iterations = random.randint(1, 1000)
+        file1_chunk_probability = random.random()
+        file2_chunk_probability = random.random()
+        file1 = generate_file(file1_iterations, file1_chunk_probability, created_chunks)
+        file2 = generate_file(file2_iterations, file2_chunk_probability, created_chunks)
+        open(path_to_save + "/file1", "w").write(file1)
+        open(path_to_save + "/file2", "w").write(file2)
+
+
+def generate_small_files_for_manual_test(directory_path):
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+    tests_to_generate = 1000
+    for i in range(tests_to_generate):
+        path_to_save = directory_path + "/test" + str(i + 1)
+        if not os.path.exists(path_to_save):
+            os.makedirs(path_to_save)
+
+        number_random_chunks = random.randint(1, 3)
+        size_chunks = random.randint(1, 10)
+        created_chunks = create_random_chunks(number_random_chunks, size_chunks, human_readable=True)
+
+        file1_iterations = random.randint(1, 100)
+        file2_iterations = random.randint(1, 100)
+        file1_chunk_probability = random.random()
+        file2_chunk_probability = random.random()
+        file1 = generate_file(file1_iterations, file1_chunk_probability, created_chunks)
+        file2 = generate_file(file2_iterations, file2_chunk_probability, created_chunks)
+        open(path_to_save + "/file1", "w").write(file1)
+        open(path_to_save + "/file2", "w").write(file2)
+
+
+def generate_very_similar_files(directory_path):
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+    tests_to_generate = 100
+    for i in range(tests_to_generate):
+        path_to_save = directory_path + "/test" + str(i + 1)
+        if not os.path.exists(path_to_save):
+            os.makedirs(path_to_save)
+
+        number_random_chunks = random.randint(1, 10)
+        size_chunks = random.randint(300, 300)
+        created_chunks = create_random_chunks(number_random_chunks, size_chunks, human_readable=True)
+
+        file1_iterations = random.randint(1, 100)
+        file2_iterations = random.randint(1, 100)
+        file1_chunk_probability = 0.95
+        file2_chunk_probability = 0.95
+        file1 = generate_file(file1_iterations, file1_chunk_probability, created_chunks)
+        file2 = generate_file(file2_iterations, file2_chunk_probability, created_chunks)
+        open(path_to_save + "/file1", "w").write(file1)
+        open(path_to_save + "/file2", "w").write(file2)
+
+
+# generate_big_files_for_automated_test("test_files/hard_files")
+generate_very_similar_files("test_files/similar_files")
--- tests/tester_script.py
+++ tests/tester_script.py
@@ -4,10 +4,21 @@ import subprocess
 
 # For all the test files we have generated through `similar_file_generator.py`
 bad_counter = 0
-for test_number in range(1, 101):
+# tests_directory = "test_files/"
+# number_tests = 100
+
+# tests_directory = "test_files/easy_files/"
+tests_directory = "/home/matheus/Development/Eiger/rolling_hash_file_diff/tests/test_files/hard_files/"
+# number_tests = 10
+number_tests = sum(os.path.isdir(tests_directory + p) for p in os.listdir(tests_directory))
+print(os.listdir(tests_directory))
+
+send_file_total_size = 0
+algorithm_total_size = 0
+for test_number in range(1, number_tests + 1):
     # Get the correspondent input files
-    file1 = "test_files/test" + str(test_number) + "/file1"
-    file2 = "test_files/test" + str(test_number) + "/file2"
+    file1 = tests_directory + "test" + str(test_number) + "/file1"
+    file2 = tests_directory + "test" + str(test_number) + "/file2"
 
     # Calls the executable passing file1 and file2 as arguments
     # Create the signature from file1
@@ -26,15 +37,17 @@ for test_number in range(1, 101):
     # print(reconstructed_file.read())
 
     # If all went well, `reconstructed_file` should be equal to `file2`
-    if filecmp.cmp(file2, "reconstructed_file"):
-        print("All went well")
-    else:
-        print("Hash collision here")
+    if not filecmp.cmp(file2, "reconstructed_file"):
+        print("Something wrong here, probably a hash collision")
         bad_counter += 1
 
     print("*********** Statistics ***********")
+    send_file_total_size += os.stat(file2).st_size
+    algorithm_total_size += os.stat('signature_file').st_size + os.stat('delta_file').st_size
     print(f"File2 size: {os.stat(file2).st_size}")
     print(f"Signature size: {os.stat('signature_file').st_size}")
     print(f"Delta size: {os.stat('delta_file').st_size}")
 
-    print("Failures: ", bad_counter)
+print("Failures: ", bad_counter)
+print(f"Total size for simply sending: {send_file_total_size}")
+print(f"Total size for algorithm: {algorithm_total_size}")
