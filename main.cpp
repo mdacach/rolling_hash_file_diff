54a2de048dc09470987ce9d3cf8f3577554bcbd2
Matheus Cardoso <dacachmatheus@gmail.com>
2022-07-25 18:27:51 -0300

    Add documentation to main driver

--- main.cpp
+++ main.cpp
@@ -8,13 +8,34 @@ auto main(int argc, const char* argv[]) -> int
     // TODO: change this to be variable
     const auto chunk_size = std::size_t{ 30 };
 
-    // We want to update `my_file` to be equal to `other_file`
-    // We first send our signature to the other machine
+    // We want to update `my_file`  to be equal to `other_file`
+    // but our network is slow, so we don't want to send big files through it.
 
-    // Other machine will compute its delta from our signature
-    // We will receive the delta and update our file
+    // The algorithm consists of:
+    // 1 - Let's compute a `signature` for `my_file`.
+    //          This signature is a much smaller file which "represents" `my_file` (not perfectly, of course).
+    // 2 - Send the `signature` for the second machine.
+    //          Remember our signature is small, so we do not use many resources to send it through the network.
+    // 3 - Second machine receives `signature` and computes a `delta` from `other_file` to `signature`.
+    //          This delta has exactly what needs to change from our `my_file` to `other_file`.
+    // 4 - Second machine sends us the `delta`.
+    //          Again, the delta is a small file.
+    // 5 - We use `delta` to update our `my_file` such that in the end it is equal to `other_file`.
+
+    // In the end, we have sent two small files through the network (instead of a potentially big one, such as the full
+    // file).
+    // The tradeoff is that we have done some computation on both our end and theirs.
+    // (We computed `signature` and used `delta` to update the file; they computed `delta`).
+    // So we are trading time (computation) for "memory" (in terms of sending the files).
+
+    using namespace std::string_literals;
+    const auto usage = "Usage must be one of: \n\
+                    ./rolling_hash_file_diff signature old-file signature-file\n\
+                    ./rolling_hash_file_diff delta signature-file new-file delta-file\n\
+                    ./rolling_hash_file_diff patch basis-file delta-file new-file\n"s;
+    // 1. Parse the user command.
     const auto command = std::string(argv[1]);
-    // TODO: We do not treat user mistakes very well here
+    // TODO: We do not treat many user mistakes nor sanitize the input.
     if (command == "signature")
     {
         const auto old_file = io_helpers::read_file_to_string(argv[2]);
@@ -40,9 +61,7 @@ auto main(int argc, const char* argv[]) -> int
     }
     else
     {
-        std::cout << "Usage must be one of: \n\
-            ./rolling_hash_file_diff signature old-file signature-file\n\
-            ./rolling_hash_file_diff delta signature-file new-file delta-file\n\
-            ./rolling_hash_file_diff patch basis-file delta-file new-file\n";
+        std::cout << usage << '\n';
+        exit(1);
     }
 }
