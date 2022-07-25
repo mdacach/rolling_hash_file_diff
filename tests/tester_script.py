7619b339dc075828e960c826cf86648b89d7e054
Matheus Cardoso <dacachmatheus@gmail.com>
2022-07-25 19:35:05 -0300

    Add some statistics to test script

--- tests/tester_script.py
+++ tests/tester_script.py
@@ -4,7 +4,7 @@ import subprocess
 
 # We have generated test cases with `similar_file_generator.py`
 bad_counter = 0  # Cases where we could not reconstruct the file -> algorithm failed
-tests_directory = "/home/matheus/Development/Eiger/rolling_hash_file_diff/tests/test_files/hard_files/"
+tests_directory = "/home/matheus/Development/Eiger/rolling_hash_file_diff/tests/test_files/similar_files/"
 # Run for each test inside the chosen directory
 number_tests = sum(os.path.isdir(tests_directory + p) for p in os.listdir(tests_directory))
 
@@ -14,6 +14,11 @@ send_file_total_size = 0
 # This is size(signature) + size(delta)
 algorithm_total_size = 0
 processed_counter = 0
+
+total_signature_file_size = 0
+total_delta_file_size = 0
+
+algorithm_better_counter = 0
 for test_number in range(1, number_tests + 1):
     # Each test case has two files, the original (file1) and the updated (file2)
     # Ultimately, we want to be able to reconstruct updated from original
@@ -44,10 +49,26 @@ for test_number in range(1, number_tests + 1):
     # TODO: Better statistics generated from tests
     send_file_total_size += os.stat(file2).st_size
     algorithm_total_size += os.stat('signature_file').st_size + os.stat('delta_file').st_size
+
+    # Analogous to having just sent the new file over the network
+    updated_file_size = os.stat(file2).st_size
+
+    # Files from the algorithm
+    signature_file_size = os.stat('signature_file').st_size
+    delta_file_size = os.stat('delta_file').st_size
+
+    total_signature_file_size += signature_file_size
+    total_delta_file_size += delta_file_size
+
     processed_counter += 1
+    if updated_file_size >= signature_file_size + delta_file_size:
+        algorithm_better_counter += 1
     print(f"#{processed_counter}/{number_tests}")
 
 print(f"Finished testing the algorithm for {number_tests} test cases in {tests_directory}");
 print("Algorithm failures: ", bad_counter)
 print(f"Total size for simply sending: {send_file_total_size}")
 print(f"Total size for algorithm: {algorithm_total_size}")
+print(f"Total size from signatures: {total_signature_file_size}")
+print(f"Total size from deltas: {total_delta_file_size}")
+print(f"Algorithm better in {algorithm_better_counter}/{number_tests} cases")
